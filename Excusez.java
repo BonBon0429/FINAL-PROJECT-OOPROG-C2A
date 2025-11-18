6e5f0a2fcf885cd2e04fd63d4e565a502171467e
Benedict Tanghal <benedict_tanghal@dlsl.edu.ph>
2025-11-18 18:57:07 +0800

    Create Excusez.java

---
+++ Excusez.java
@@ -0,0 +1,8 @@
+public class Excusez {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("idk what to put here");
+        System.out.println("ewan what are you doing here?");
+    }
+    
+}
