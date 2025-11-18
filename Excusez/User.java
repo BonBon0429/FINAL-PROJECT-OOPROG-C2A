0740f1f06f5d9137b4d7bc88c11e825962cbd48f
yuan <yuan_benedict_chavez@dlsl.edu.ph>
2025-11-18 21:10:25 +0800

    Update User.java

--- Excusez/User.java
+++ Excusez/User.java
@@ -1,8 +1,16 @@
 package Excusez;
 
-enum LetterStatus {
-    PENDING, APPROVED, REJECTED
-}
+import java.util.ArrayList;
 
-
-
+public abstract class User {
+    protected int userId;
+    protected String name;
+    protected String email;
+    protected String password;
+    
+    public User(int userId, String name, String email, String password) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
