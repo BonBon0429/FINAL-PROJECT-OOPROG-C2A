e7dd885623129300bb4cc3529ea43ba9b511b126
Erith1 <lorenzo.lubrico@gmail.com>
2025-11-18 21:43:45 +0800

    Carried teacher.java to package teacher.java

--- Excusez/Teacher.java
+++ Excusez/Teacher.java
@@ -7,8 +7,32 @@ public class Teacher extends Excusez{
     private String subject;
     
     public Teacher(int userId, String name, String email, String password, String subject) {
-        super(userId, name, email, password);
-        this.subject = subject;
+    super(userId, name, email, password);
+    this.subject = subject;
     }
 
+public ArrayList<ExcuseLetter> reviewLetters() {
+    ArrayList<ExcuseLetter> pendingLetters = new ArrayList<>();
+    for (ExcuseLetter letter : Excusez.excuseLetters) {
+    if (letter.getStatus() == LetterStatus.PENDING) {
+    pendingLetters.add(letter);
+    }
+    }
+        return pendingLetters;
+    }
+    public void rejectLetter(int letterId, String comments) {
+        for (ExcuseLetter letter : Excusez.excuseLetters) {
+            if (letter.getLetterId() == letterId) {
+                letter.updateStatus(LetterStatus.REJECTED, this.name, comments);
+                System.out.println("✓ Letter " + String.format("%04d", letterId) + " rejected.");
+                return;
+            }
+        }
+        System.out.println("Letter not found!");
+    }
+
+    @Override
+    public ArrayList<ExcuseLetter> viewExcuseHistory() {
+        return new ArrayList<>(Excusez.excuseLetters);
+    }
 }
