9a905efa5f90f9577f41836e7061a5032e78a8d5
yuan <yuan_benedict_chavez@dlsl.edu.ph>
2025-11-18 20:09:33 +0800

    Create Teacher.java

---
+++ Excusez/Teacher.java
@@ -0,0 +1,14 @@
+package Excusez;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Teacher extends Excusez{
+    private String subject;
+    
+    public Teacher(int userId, String name, String email, String password, String subject) {
+        super(userId, name, email, password);
+        this.subject = subject;
+    }
+
+}
