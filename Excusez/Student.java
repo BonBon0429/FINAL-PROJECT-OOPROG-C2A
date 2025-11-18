9ae30fbac408bc85f0727f6cfb81b5c572609649
yuan <yuan_benedict_chavez@dlsl.edu.ph>
2025-11-19 00:06:38 +0800

    Update Student.java

--- Excusez/Student.java
+++ Excusez/Student.java
@@ -3,3 +3,12 @@ package Excusez;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+public class Student extends User implements Submitter {
+    private int yearLevel;
+    private String section;
+    
+    public Student(int userId, String name, String email, String password, int yearLevel, String section) {
+        super(userId, name, email, password);
+        this.yearLevel = yearLevel;
+        this.section = section;
+    }
