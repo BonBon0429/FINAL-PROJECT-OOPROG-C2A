1fad6067763dd3a1dd7b157745705efd9a984680
Erith1 <lorenzo.lubrico@gmail.com>
2025-11-18 19:53:36 +0800

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -3,5 +3,11 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Teacher extends Excusez{
+    private String subject;
     
+    public Teacher(int userId, String name, String email, String password, String subject) {
+        super(userId, name, email, password);
+        this.subject = subject;
+    }
+
 }
