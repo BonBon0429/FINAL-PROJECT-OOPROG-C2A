8fab15c00e3fd0ae7ad1878e5eb841ccea39db6e
Erith1 <lorenzo.lubrico@gmail.com>
2025-11-18 20:17:31 +0800

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -6,8 +6,17 @@ public class Teacher extends Excusez{
     private String subject;
     
     public Teacher(int userId, String name, String email, String password, String subject) {
-        super(userId, name, email, password);
-        this.subject = subject;
+    super(userId, name, email, password);
+    this.subject = subject;
     }
 
+public ArrayList<ExcuseLetter> reviewLetters() {
+    ArrayList<ExcuseLetter> pendingLetters = new ArrayList<>();
+    for (ExcuseLetter letter : Excusez.excuseLetters) {
+    if (letter.getStatus() == LetterStatus.PENDING) {
+    pendingLetters.add(letter);
+    }
+    }
+        return pendingLetters;
+    }
 }
